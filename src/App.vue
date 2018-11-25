af20315df20186c4fd3a057ec5a3d4b740650bf7
jeffholst <jeffholst@gmail.com>
2018-11-25 13:00:34 -0600

    Updated about

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,9 @@
       <md-dialog-title>CF Linda</md-dialog-title>
       <div class="container">
         <a href="http://www.jeffholst.net" target="_blank"><img alt="Logo" src="./assets/jd-vue-logo.png" style="display: block; margin-left: auto; margin-right: auto;"></a>
-        <p>Built at <a href="http://www.jeffholst.net" target="_blank">Jeff's DŌJŌ</a> using <a href="https://vuejs.org/" target="_blank">Vue.js</a> and styled with <a href="https://vuematerial.io/" target="_blank">Material Design</a>.
-        <br><br><a href="https://github.com/jeffholst/cf-linda/blob/master/LICENSE" target="_blank">MIT License</a> Copyright (c) 2018 <a href="http://www.jeffholst.net/" target="_blank">Jeff Holst</a></p>
-        <p><a href= "https://www.crossfit.com/workout/2003/07/05#/comments" target="_blank">CrossFit Linda Reference</a></p>
+        <p>Built at <a href="http://www.jeffholst.net" target="_blank">Jeff's DŌJŌ</a> using <a href="https://vuejs.org/" target="_blank">Vue.js</a> and styled with <a href="https://vuematerial.io/" target="_blank">Material Design</a>.</p>
+        <p><a href="https://github.com/jeffholst/cf-linda" target="_blank">GitHub Repository</a> | <a href= "https://www.crossfit.com/workout/2003/07/05#/comments" target="_blank">CrossFit Linda Reference</a></p>
+         <p><a href="https://github.com/jeffholst/cf-linda/blob/master/LICENSE" target="_blank">MIT License</a>. Copyright (c) 2018 <a href="http://www.jeffholst.net/" target="_blank">Jeff Holst</a></p>
       </div>
       <md-dialog-actions>
         <md-button class="md-primary" @click="showAbout = false">Close</md-button>
