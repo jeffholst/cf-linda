dda486adfbe5ba3247d24fca67b98ee630bef5d3
jeffholst <jeffholst@gmail.com>
2018-11-25 12:52:48 -0600

    Logo, license, & About

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,11 @@
       <md-button @click="showAbout = true">About</md-button>
     </md-toolbar>
     <md-dialog :md-active.sync="showAbout">
-      <md-dialog-title>About</md-dialog-title>
+      <md-dialog-title>CF Linda</md-dialog-title>
       <div class="container">
-        <a href="https://vuejs.org/" target="_blank"><img alt="Vue logo" src="./assets/logo.png" style="display: block; margin-left: auto; margin-right: auto;"></a>
-        <p><a href="https://vuejs.org/" target="_blank">Vue.js</a> application using <a href="https://vuematerial.io/" target="_blank">Material Design</a>.
-        <br><br>By Jeff Holst</p>
+        <a href="http://www.jeffholst.net" target="_blank"><img alt="Logo" src="./assets/jd-vue-logo.png" style="display: block; margin-left: auto; margin-right: auto;"></a>
+        <p>Built at <a href="http://www.jeffholst.net" target="_blank">Jeff's DŌJŌ</a> using <a href="https://vuejs.org/" target="_blank">Vue.js</a> and styled with <a href="https://vuematerial.io/" target="_blank">Material Design</a>.
+        <br><br><a href="https://github.com/jeffholst/cf-linda/blob/master/LICENSE" target="_blank">MIT License</a> Copyright (c) 2018 <a href="http://www.jeffholst.net/" target="_blank">Jeff Holst</a></p>
         <p><a href= "https://www.crossfit.com/workout/2003/07/05#/comments" target="_blank">CrossFit Linda Reference</a></p>
       </div>
       <md-dialog-actions>
