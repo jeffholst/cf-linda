057506dd23dd9a5b65cd514adb7f6ed6b6b9a886
jeffholst <jeffholst@gmail.com>
2018-11-26 00:05:41 -0600

    Added gzip

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 // vue.config.js
+const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-    ? '/cf-linda/'
-    : '/'
+  baseUrl: process.env.NODE_ENV === 'production' ? '/cf-linda/' : '/',
+  configureWebpack: {
+    plugins: [
+      new CompressionPlugin()
+    ]
   }
+}
+
+  
